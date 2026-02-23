c53e348b2800c753ba14711c59f0927595a54bdf
John Brandews <johnbrandews@gmail.com>
2026-02-24 01:37:47 +0300

    added changes

--- app/api/users/upload-profile-image/route.ts
+++ app/api/users/upload-profile-image/route.ts
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
 import { NextRequest, NextResponse } from 'next/server';
 import { getCurrentUser } from '@/lib/auth';
 import crypto from 'crypto';
